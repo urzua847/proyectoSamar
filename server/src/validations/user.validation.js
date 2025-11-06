8c76948ab847318825d2532a8818b40d897f0781
urzua847 <urzua847@gmail.com>
2025-11-06 10:28:48 -0300

    wip

--- server/src/validations/user.validation.js
+++ server/src/validations/user.validation.js
@@ -6,7 +6,7 @@ export const userQueryValidation = Joi.object({
   email: Joi.string().email(),
   rut: Joi.string(),
 })
-  .or("id", "email", "rut") // Debe venir al menos uno de estos
+  .or("id", "email", "rut") 
   .messages({
     "object.missing": "Debes proporcionar al menos un parámetro: id, email o rut.",
   });
@@ -14,9 +14,9 @@ export const userQueryValidation = Joi.object({
 export const userBodyValidation = Joi.object({
   nombreCompleto: Joi.string().min(3).max(50),
   email: Joi.string().email(),
-  newPassword: Joi.string().min(6).allow(''), // Permite que la nueva contraseña esté vacía
+  newPassword: Joi.string().min(6).allow(''), 
   rut: Joi.string(),
   rol: Joi.string(),
-  password: Joi.string(), // Para verificar la contraseña actual si se cambia
+  password: Joi.string(), 
 })
   .or("nombreCompleto", "email", "newPassword", "rut", "rol");
